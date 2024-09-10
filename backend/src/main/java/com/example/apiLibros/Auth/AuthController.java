28e4629025f4be187d24077a2f6cce03e823933e
Blukshark <87742693+Blukshark@users.noreply.github.com>
2024-09-09 22:34:17 -0300

    Back y Front conectados
    Ahora el Front puede enviar una peticion al back-end, validar usuario y contraseña, y si es correcto, te envia directamente al dashboard. El Back end permite peticiones del puerto 8081, que empiecen por la ruta /api.

--- backend/src/main/java/com/example/apiLibros/Auth/AuthController.java
+++ backend/src/main/java/com/example/apiLibros/Auth/AuthController.java
@@ -5,15 +5,12 @@ import com.example.apiLibros.Cliente.ClienteRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/api/auth")
+//@CrossOrigin(origins = "http://localhost:8081") // Permitir peticiones desde tu frontend
 public class AuthController {
-
     @Autowired
     private ClienteRepository clienteRepo;
 
