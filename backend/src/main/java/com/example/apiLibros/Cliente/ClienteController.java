50340a4663e15f6c2e4040c588b4cb947e2f8aa2
Blukshark <87742693+Blukshark@users.noreply.github.com>
2024-09-12 14:03:47 -0300

    Backend que añade el corsConfig
    Se implementa un controlador de CorsConfig para poder recibir peticiones del frontend (p8081) al back (p8080)

--- backend/src/main/java/com/example/apiLibros/Cliente/ClienteController.java
+++ backend/src/main/java/com/example/apiLibros/Cliente/ClienteController.java
@@ -11,36 +11,36 @@ import java.util.List;
 @RequestMapping("api/clientes")
 public class ClienteController {
     @Autowired
-    private ClienteRepository clienteRepository;
+    private ClienteRepository clienteRepo;
 
     // Obtener todos los clientes
     @GetMapping
     public List<Cliente> getAllClientes() {
-        return clienteRepository.findAll();
+        return clienteRepo.findAll();
     }
 
     // Obtener un cliente segun su id
     @GetMapping("/{id}")
     public Cliente getClienteById(@PathVariable Long id) {
-        return clienteRepository.findById(id).orElse(null);
+        return clienteRepo.findById(id).orElse(null);
     }
 
     // Crear un nuevo cliente
     @PostMapping
     public Cliente createCliente(@RequestBody Cliente cli) {
-        return clienteRepository.save(cli);
+        return clienteRepo.save(cli);
     }
 
     // Actualizar un cliente existente
     @PutMapping("/{id}")
     public ResponseEntity<String> updateCliente(@PathVariable Long id, @RequestBody Cliente cli) {
-        Cliente existingCliente = clienteRepository.findById(id).orElse(null);
+        Cliente existingCliente = clienteRepo.findById(id).orElse(null);
         if (existingCliente != null) {
             existingCliente.setNombre(cli.getNombre());
             existingCliente.setApellido(cli.getApellido());
             existingCliente.setUsername(cli.getUsername());
             existingCliente.setPassword(cli.getPassword());
-            clienteRepository.save(existingCliente);
+            clienteRepo.save(existingCliente);
             return ResponseEntity.ok("Cliente actualizado con éxito.");
         } else {
             return ResponseEntity.status(HttpStatus.NOT_FOUND).body("Cliente no encontrado.");
@@ -50,11 +50,21 @@ public class ClienteController {
     // Eliminar un cliente
     @DeleteMapping("/{id}")
     public String deleteCliente(@PathVariable Long id) {
-        if (clienteRepository.existsById(id)) {
-            clienteRepository.deleteById(id);
+        if (clienteRepo.existsById(id)) {
+            clienteRepo.deleteById(id);
             return "Cliente eliminado con éxito.";
         }
         return "Cliente no encontrado.";
+    }
+
+    @PostMapping("/login")
+    public ResponseEntity<String> login(@RequestBody Cliente cli) {
+        Cliente existingCliente = clienteRepo.findByUsername(cli.getUsername());
+        if (existingCliente != null && existingCliente.getPassword().equals(cli.getPassword())) {
+            return ResponseEntity.ok("Inicio de sesión exitoso.");
+        } else {
+            return ResponseEntity.status(HttpStatus.UNAUTHORIZED).body("Credenciales incorrectas.");
+        }
     }
 
 }
--- backend/src/main/java/com/example/apiLibros/Cliente/ClienteRepository.java
+++ backend/src/main/java/com/example/apiLibros/Cliente/ClienteRepository.java
@@ -5,4 +5,6 @@ import org.springframework.stereotype.Repository;
 
 @Repository
 public interface ClienteRepository extends JpaRepository<Cliente,Long> {
+    Cliente findByUsername(String username);
+    //como el nombre indica, asi como busco por id, puedo buscar por nombre de usuario
 }
